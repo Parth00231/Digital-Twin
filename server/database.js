cb344a67873b483e2744d0094d28cfa92c155e8e
Parth Rastogi <parth@Parths-MacBook-Air.local>
2026-05-03 17:03:00 +0530

    fix db connection settings

--- server/database.js
+++ server/database.js
@@ -3,13 +3,18 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
-  connectionLimit: 2,
+  connectionLimit: 1,
+  queueLimit: 0,
+  connectTimeout: 60000,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
